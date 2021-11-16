223fe770e31220d912194ee745d5b8af304dc7a4
k.greesuradej <world1955@hotmail.com>
2021-11-16 23:31:06 +0700

    looked at gravity, but not yet working

--- classes/particles.js
+++ classes/particles.js
@@ -1,18 +1,47 @@
 class Particle {
-  _position = null;
-  _velocity = null;
-  _gravity = null;
+  position = null;
+  velocity = null;
+  gravity = null;
+  mass = 1;
 
   constructor(x, y, magnitude, direction, gravity) {
-    this._position = new Vector(x, y);
-    this._velocity = new Vector(0, 0);
-    this._velocity.setLength(magnitude);
-    this._velocity.setAngle(direction);
-    this._gravity = new Vector(0, gravity || 0);
+    this.position = new Vector(x, y);
+    this.velocity = new Vector(0, 0);
+    this.velocity.setLength(magnitude);
+    this.velocity.setAngle(direction);
+    this.gravity = new Vector(0, gravity || 0);
   }
+
   update() {
     //This line makes stuff accelerates
-    this._velocity = this._velocity.add(this._gravity);
-    this._position = this._position.add(this._velocity);
+    this.velocity = this.velocity.add(this.gravity);
+    this.position = this.position.add(this.velocity);
+  }
+
+  angleTo(p) {
+    console.log(p.position.getY() - this.position.getY());
+    return Math.atan2(
+      p.position.getY() - this.position.getY(),
+      p.position.getX() - this.position.getX()
+    );
+  }
+
+  distanceTo(p) {
+    return Math.sqrt(
+      Math.pow(p.position.getX() - this.position.getX(), 2) +
+        Math.pow(p.position.getY() - this.position.getY(), 2)
+    );
+  }
+
+  gravitateTo(p) {
+    const gravity = new Vector(0, 0);
+    const angle = this.angleTo(p);
+    const distance = this.distanceTo(p);
+
+    //We're not using the gravitational constant here because we're not trying to simulate the real world
+    gravity.setLength(p.mass / distance ** 2);
+    gravity.setAngle(angle);
+
+    return gravity;
   }
 }
