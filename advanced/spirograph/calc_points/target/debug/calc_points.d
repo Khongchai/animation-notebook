40f501c7f7369d132e561254287c20d0a1ef3d33
khongchai <world1955@hotmail.com>
2022-11-06 15:48:15 +0700

    removed cargo stuff

--- advanced/spirograph/calc_points/target/debug/calc_points.d
+++
@@ -1,1 +0,0 @@
-D:\coding\ notes\animation-notebook\advanced\spirograph\calc_points\target\debug\calc_points.exe: D:\coding\ notes\animation-notebook\advanced\spirograph\calc_points\src\main.rs
--- advanced/spirograph/calc_points/target/debug/deps/calc_points-c24a79b0b4f1c41d.d
+++
@@ -1,5 +0,0 @@
-D:\coding notes\animation-notebook\advanced\spirograph\calc_points\target\debug\deps\calc_points-c24a79b0b4f1c41d.exe: src\main.rs
-
-D:\coding notes\animation-notebook\advanced\spirograph\calc_points\target\debug\deps\calc_points-c24a79b0b4f1c41d.d: src\main.rs
-
-src\main.rs:
