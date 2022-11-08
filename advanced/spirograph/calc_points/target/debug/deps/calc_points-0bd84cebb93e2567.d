537bf23e6755b5cc0b8e0fbc9d18ae97f4267369
khongchai <world1955@hotmail.com>
2022-11-08 23:44:32 +0700

    fixed bugs

--- advanced/spirograph/calc_points/target/debug/deps/calc_points-0bd84cebb93e2567.d
+++
@@ -1,5 +0,0 @@
-d:\coding notes\animation-notebook\advanced\spirograph\calc_points\target\debug\deps\calc_points-0bd84cebb93e2567.rmeta: src\main.rs
-
-d:\coding notes\animation-notebook\advanced\spirograph\calc_points\target\debug\deps\calc_points-0bd84cebb93e2567.d: src\main.rs
-
-src\main.rs:
