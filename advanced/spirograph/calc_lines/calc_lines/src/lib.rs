59fdfee5a9b606c6d950234d9f2d3bf0bef4871c
khongchai <world1955@hotmail.com>
2022-12-04 19:14:27 +0700

    test done

--- advanced/spirograph/calc_lines/calc_lines/src/lib.rs
+++ advanced/spirograph/calc_lines/calc_lines/src/lib.rs
@@ -7,13 +7,13 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn draw_lines() -> Box<[f64]> {
+pub fn draw_lines(data: JsValue, theta: f64, rod_length: f64) -> Box<[f64]> {
     //  TODO
     return Box::new([]);
 }
 
 // TODO write test first
-fn compute_epitrochoid(data: &Box<[f64]>, theta: f64, rod_length: f64) -> [f64; 2] {
+pub fn compute_epitrochoid(data: &Vec<Vec<f64>>, theta: f64, rod_length: f64) -> [f64; 2] {
     println!("{:?}, {}, {}", data, theta, rod_length);
 
     //TODO return vec2
@@ -23,43 +23,41 @@ fn compute_epitrochoid(data: &Box<[f64]>, theta: f64, rod_length: f64) -> [f64; 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::Value;
+    use serde_json::{Map, Value};
     use std::fs;
 
     #[test]
     fn compute_epitrochoid_test() {
-        let data = fs::read_to_string("test_data.json").expect("Unable to read the test json file");
+        let data: String =
+            fs::read_to_string("test_data.json").expect("Unable to read the test json file");
         let values: Value =
             serde_json::from_str(&data).expect("Unable to parse json into rust value");
 
         for value in values.as_array().unwrap() {
-            let args = value["args"].as_object().unwrap();
-            let theta = args["theta"].as_f64().unwrap();
-            let rod_length = args["rodLength"].as_f64().unwrap();
-            println!(
-                "{:?}",
-                //TODO box::from from this.
-                args["data"]
-                    .as_array()
-                    .unwrap()
-                    .iter()
-                    .map(|e| e
-                        .as_array()
+            let args: &Map<String, Value> = value["args"].as_object().unwrap();
+            let theta: f64 = args["theta"].as_f64().unwrap();
+            let rod_length: f64 = args["rodLength"].as_f64().unwrap();
+
+            let data: Vec<Vec<f64>> = args["data"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|e| {
+                    e.as_array()
                         .unwrap()
                         .iter()
                         .map(|e| e.as_f64().unwrap())
-                        .collect::<Vec<f64>>())
-                    .collect::<Vec<Vec<f64>>>()
+                        .collect::<Vec<f64>>()
+                })
+                .collect::<Vec<Vec<f64>>>();
+
+            let computation_result = compute_epitrochoid(&data, theta, rod_length);
+            let result = value["result"].as_object().unwrap();
+
+            assert!(
+                computation_result[0] == result["x"].as_f64().unwrap()
+                    && computation_result[1] == result["y"].as_f64().unwrap()
             );
-            // let data: Box<[f64]> = Box::from(
-            //     args["data"]
-            //         .as_array()
-            //         .unwrap()
-            //         .into_iter()
-            //         .map(|e| e.as_f64().unwrap())
-            //         .collect::<Vec<f64>>(),
-            // );
-            // compute_epitrochoid(&data, theta, rod_length);
         }
     }
 }
