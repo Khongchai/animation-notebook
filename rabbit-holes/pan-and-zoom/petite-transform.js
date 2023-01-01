bf2101507b949b536cb439712711140129e86d16
khongchai <world1955@hotmail.com>
2023-01-01 20:32:45 +0700

    basic of the lib done

--- rabbit-holes/pan-and-zoom/petite-transform.js
+++ rabbit-holes/pan-and-zoom/petite-transform.js
@@ -1,11 +1,66 @@
-// TODO Move everything here first and then throttle.
+// TODO possible feature:
+// Simplify API find a way to simplify things further
+// Managed mode, where the lib manages all canvas transformations.
 class PetiteTransform {
-  #getCurrentScale = () => 0;
-  #draw = () => {};
-  #globalOffset = {
+  /**
+   * @type {() => {x: number, y: number, z: number}} `x` is the x offset, `y` the y offset, and `z` the zoom scale.
+   */
+  #getTransformReference = () => {
+    x: 0;
+    y: 0;
+    z: 0;
+  };
+  // TODO
+  #easeFactor = 1;
+  #cumulatedTransform = {
     dx: 0,
     dy: 0,
     dz: 1,
+    isSet: false,
+    /**
+     * Self-explanatory.
+     *
+     * @type {(x?: number, y?: number, z?: number) => void}
+     */
+    setTransform: function (x = this.dx, y = this.dy, z = this.dz) {
+      this.isSet = true;
+      this.dx = x;
+      this.dy = y;
+      this.dz = z;
+    },
+    /**
+     * Like `setTransform`, but instead of replacing the values, increment them instead.
+     *
+     * This is to cache all values in-between frames when called from an animation loop.
+     *
+     * @type {(x?: number, y?: number, z?: number) => void}
+     */
+    setTransformIncrement: function (x = 0, y = 0, z = 0) {
+      this.isSet = true;
+      this.dx += x;
+      this.dy += y;
+      this.dz += z;
+    },
+    /**
+     * Spoonfeed the client only once and then close until more values are set.
+     * This is to prevent any translation method in an animation frame from applying the
+     * same transform twice. If you need the same transform in multiple places, store it in a variable somewhere.
+     *
+     * @type {() => {dx: number, dy: number, dz: number}}
+     */
+    getTransform: function () {
+      if (this.isSet) {
+        this.isSet = false;
+        const returnVal = { dx: this.dx, dy: this.dy, dz: this.dz };
+        this.dx = 0;
+        this.dy = 0;
+        this.dz = 1;
+
+        return returnVal;
+      }
+
+      return { dx: 0, dy: 0, dz: 1 };
+    },
   };
   #isMouseDown = false;
   #panOffset = {
@@ -18,35 +73,88 @@ class PetiteTransform {
       y: 0,
     },
   };
-  #zoomScale = {
-    prev: 1,
-    cur: 1,
-  };
+  /**
+   * @type {{type: string, callback: () => any}[]}
+   */
+  #listenersRefs = [];
 
   /**
    *
-   * @param {() => number} currentScale a callback that returns the current scale factor of the canvas.
-   * @param {() => any} draw a callback that updates the screen. This method will be throttled to match the frame rate of the browser
-   *                    as if it is running in a `requestAnimationFrame` loop.
+   * @param {() => number} transformReference a callback that returns the current transform of the canvas.
+   * @param {number} easeFactor the t in (a + (b - a) * t) of the linear interpolation equation.
+   * @param {number?} devicePixelRatio The device pixel ratio that you set your canvas to. It is
+   * vital that this matches what you have set for your canvas.
    */
-  constructor(currentScale, draw) {
-    this.#draw = draw;
-    this.#getCurrentScale = currentScale;
-    this.#globalOffset = {
-      dx: 0,
-      dy: 0,
-      dz: 1,
-    };
+  constructor(transformReference, easeFactor, devicePixelRatio = 1) {
+    this.#easeFactor = easeFactor;
+    this.#getTransformReference = transformReference;
+    this.#cumulatedTransform.setTransform(0, 0, 1);
 
-    // TODO setup event listeners and clear when appropriate.
-  }
+    this.#addEventListener("mousedown", (e) => {
+      this.#onmousedown({
+        x: e.x * devicePixelRatio,
+        y: e.y * devicePixelRatio,
+      });
+    });
 
-  get currentTransform() {
-    return { ...this.#globalOffset };
+    this.#addEventListener("mousemove", (e) => {
+      this.#onmousemove({
+        x: e.x * devicePixelRatio,
+        y: e.y * devicePixelRatio,
+      });
+    });
+
+    this.#addEventListener("mouseup", (e) => {
+      this.#onmouseup({
+        x: e.x * devicePixelRatio,
+        y: e.y * devicePixelRatio,
+      });
+    });
+
+    this.#addEventListener(
+      "wheel",
+      (e) => {
+        e.preventDefault();
+
+        this.#onwheel({
+          x: e.x * devicePixelRatio,
+          y: e.y * devicePixelRatio,
+          deltaY: e.deltaY,
+        });
+      },
+      { passive: false }
+    );
   }
 
   /**
-   * @param {MouseEvent} e
+   * Call this when you would like to remove all event listeners.
+   */
+  dispose() {
+    this.#listenersRefs.forEach((obj) =>
+      document.removeEventListener(obj.type, obj.callback, obj.options)
+    );
+  }
+
+  /**
+   * Returns a change in the transform.
+   *
+   * Note that if you use a method that resets the transformation matrix
+   * like `ctx.setTransform` instead of something like `ctx.transform` or
+   * `ctx.translate`, the transformations will not work correctly.
+   *
+   * @type {() => {dx: number, dy: number, dz: number}}
+   */
+  get currentTransform() {
+    return { ...this.#cumulatedTransform };
+  }
+
+  #addEventListener(type, callback, options) {
+    document.addEventListener(type, callback, options);
+    this.#listenersRefs.add({ type, callback, options });
+  }
+
+  /**
+   * @param {{x: number, y: number}} e
    */
   #onmousedown(e) {
     this.#isMouseDown = true;
@@ -59,115 +167,35 @@ class PetiteTransform {
   }
 
   /**
-   * @param {MouseEvent} e
+   * @param {{x: number, y: number}} e
    */
   #onmousemove(e) {
     if (this.#isMouseDown) {
-      const dx = e.x - this.#panOffset.prev.x;
-      const dy = e.y - this.#panOffset.prev.y;
+      const dx = e.x - this.prev.x;
+      const dy = e.y - this.prev.y;
 
-      this.#globalOffset.dx = dx / this.#getCurrentScale();
-      this.#globalOffset.dy = dy / this.#getCurrentScale();
-      this.#panOffset.prev.x = e.x;
-      this.#panOffset.prev.y = e.y;
+      const globalDx = dx / this.#getTransformReference().z;
+      const globalDy = dy / this.#getTransformReference().z;
+      this.prev.x = e.x;
+      this.prev.y = e.y;
 
-      this.#throttledDraw();
-
-      //TODO will the throttling affect
-      this.#globalOffset.dx = 0;
-      this.#globalOffset.dy = 0;
+      this.#cumulatedTransform.setTransformIncrement(globalDx, globalDy);
     }
   }
 
   /**
-   *
-   * @param {MouseEvent} e
+   * @param {{x: number, y: number, deltaY: number}} e
    */
   #onwheel(e) {
     const change = -e.deltaY * 0.0005;
-    this.#globalOffset.dz = 1 + change;
-    this._calculateZoomOffset(this.#getCurrentScale(), change, e.x, e.y);
+    this.#cumulatedTransform.dz = 1 + change;
+    const { x, y, z } = this.#getTransformReference();
 
-    this.#throttledDraw();
+    // Grab the world space position of the cursor so that we can calculate
+    // later how much to offset the canvas by after zooming.
+    const wx = (e.x - x) / z;
+    const wy = (e.y - y) / z;
 
-    // reset the offset dz
-    globalOffset.dz = 1;
+    this.#cumulatedTransform.setTransform(-wx * change, -wy * change);
   }
-
-  // TODO
-  #throttledDraw() {}
 }
-
-// Tiny code snippet that can be used to obtain the transform of the canvas.
-// TODO
-const globalOffset = {
-  dx: 0,
-  dy: 0,
-  dz: 1,
-};
-
-const _panOffset = {
-  prev: {
-    x: 0,
-    y: 0,
-  },
-  // For keeping track of how much translation we have already done to the canvas.
-  cur: {
-    x: 0,
-    y: 0,
-  },
-  onmousedown: function (e) {
-    this.isDown = true;
-    this.prev.x = e.x - this.cur.x;
-    this.prev.y = e.y - this.cur.y;
-  },
-  onmouseup: function (e) {
-    this.isDown = false;
-  },
-  onmousemove: function (e) {
-    if (this.isDown) {
-      const dx = e.x - this.prev.x;
-      const dy = e.y - this.prev.y;
-
-      globalOffset.dx = dx / zoomOffset.getCurrentZoomLevel();
-      globalOffset.dy = dy / zoomOffset.getCurrentZoomLevel();
-      this.prev.x = e.x;
-      this.prev.y = e.y;
-
-      draw();
-
-      globalOffset.dx = 0;
-      globalOffset.dy = 0;
-    }
-  },
-  isDown: false,
-};
-
-const _zoomOffset = {
-  prev: 1,
-  cur: 1,
-  // Assume that .a and .d are the same
-  getCurrentZoomLevel: () => ctx.getTransform().a,
-
-  // Instead of referring to the globalOffset, refer to ctx's current transformation.
-  // Basically, that's like the source of truth now.
-  _calculateZoomOffset: function (currentZoom, zoomChange, sx, sy) {
-    // Get the previous transform.
-    const { e, f } = ctx.getTransform();
-    const wx = (sx - e) / currentZoom;
-    const wy = (sy - f) / currentZoom;
-    console.log(wx);
-    globalOffset.dx = -wx * zoomChange;
-    globalOffset.dy = -wy * zoomChange;
-  },
-  onwheel: function (e) {
-    let change = -e.deltaY * 0.0005;
-    globalOffset.dz = 1 + change;
-    this._calculateZoomOffset(this.getCurrentZoomLevel(), change, e.x, e.y);
-
-    draw();
-
-    // reset the offset dz
-    globalOffset.dz = 1;
-  },
-};
